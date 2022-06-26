b278844cc57f774fd22654241990581e33422cdc
nightmare324 <93826447+nightmare324@users.noreply.github.com>
2022-06-26 21:54:27 +0300

    Add files via upload

--- all.bat
+++ all.bat
@@ -1,6 +1,34 @@
-
 @ECHO OFF
 echo MADE BY FADI LMAO ITS OP
+::Run as Admin
+Reg.exe add HKLM /F >nul 2>&1
+if %errorlevel% neq 0 start "" /wait /I /min powershell -NoProfile -Command start -verb runas "'%~s0'" && exit /b
+:Check for updates
+set local=0.1
+set localtwo=%local%
+if exist "%temp%\Updater.bat" DEL /S /Q /F "%temp%\Updater.bat" >nul 2>&1
+curl -g -L -o "%temp%\Updater.bat" "https://raw.githubusercontent.com/auraside/HoneCtrl/main/Files/HoneCtrlVer" >nul 2>&1
+call "%temp%\Updater.bat"
+IF "%local%" gtr "%localtwo%" (
+	cls
+	Mode 65,16
+	echo      [Y] Yes, Update
+	echo      [N] No
+	echo.
+	choice /c:YN /n /m "%DEL%                                >:"
+	set choice=!errorlevel!
+	if !choice! equ 1 (
+		curl -L -o "C:\Users\%username%\Documents\all.bat" "https://github.com/auraside/HoneCtrl/releases/latest/download/HoneCtrl.Bat"
+		start "all" "C:\Users\%username%\Documents\all.bat"
+		del %0
+		exit /b
+	)
+	Mode 130,45
+)
+
+>nul 2>&1 call "C:\Hone\HoneRevert\firstlaunch.bat"
+if %firstlaunch%==0 (goto start)
+:start
 SET choice=
 SET /p choice=What color u prefer? b=Blue g=Green r=Red y=Yellow p=Purple :
 IF NOT '%choice%'=='' SET choice=%choice:~0,1%
